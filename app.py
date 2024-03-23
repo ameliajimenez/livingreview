130d115650155bbb96652a7c3e8f6cd867f8d44b
Amelia <amji@itu.dk>
2024-03-23 12:46:34 +0100

    add pictures and 2 column layout

--- app.py
+++ app.py
@@ -18,16 +18,31 @@ def convert_key_to_paper_name(str):
 
 st.set_page_config(layout="wide")
 
-# Add a title and intro text
-st.title('Dataset Explorer :dizzy:')
-st.text('This is a web app to allow exploration of medical imaging datasets')
+col4, col5 = st.columns(2)
+
+with col4:
+    # Add a title and intro text
+    st.title('Dataset Explorer :dizzy:')
+    st.text('This is a web app to allow exploration of medical imaging datasets')
+
+    st.image("overview.png", width=600)
 
 conn = st.connection("postgresql", "sql")
 
 # Perform query.
 df_datasets = conn.query('SELECT * FROM datasets', ttl="10m")
 
-selected_dataset = st.selectbox('Select dataset', options=df_datasets.name)
+with col5:
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
+    selected_modality = st.selectbox('Select modality', options=df_datasets.modality.unique())
+    df_datasets_filtered = df_datasets.loc[df_datasets['modality'] == selected_modality]
+    st.text("")
+    selected_dataset = st.selectbox('Select dataset', options=df_datasets_filtered.name)
+    #selected_dataset = st.selectbox('Select dataset', options=df_datasets.name)
 
 #df_data = conn.query('SELECT * FROM datasets where name = :thename', ttl="10m", params={"thename": selected_dataset})
 
@@ -40,7 +55,9 @@ df['paper_name'] = df['name'].apply(convert_key_to_paper_name)
 df_shortcuts = df[['name', 'paper_name', 'arxiv', 'shortcuts']].dropna()
 df_labels = df[['name', 'paper_name', 'arxiv', 'labels']].dropna()
 
-st.header(selected_dataset)
+st.header("Exploring...")
+st.subheader(selected_dataset + " dataset")
+#st.header(selected_dataset)
 if not df.empty:
     #annotated_text("Jiménez-Sánchez et al. 2023 [link](https://arxiv.org/abs/2402.06353) ", ("chest drains", "shortcuts"))
     #df_shortcuts.apply(lambda x: st.markdown("check out this [link](%s)" % x.arxiv), axis=1)
@@ -51,16 +68,16 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("Shortcuts")
+   st.image("shortcuts.png")
    df_shortcuts.apply(
        lambda x: annotated_text(x.paper_name, ' [ref](%s) ' % x.arxiv, (x.shortcuts, '', "#faa")), axis=1)
    #st.dataframe(df_shortcuts.set_index(df_shortcuts.columns[0]))
-   st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
    st.header("Additional labels")
+   st.image("labels.png")
    df_labels.apply(lambda x: annotated_text(x.paper_name, ' [ref](%s) ' % x.arxiv, (x.labels, '', '#9cf')), axis=1)
-   st.image("https://static.streamlit.io/examples/dog.jpg")
 
 with col3:
    st.header("Additional resources")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+   st.image("embedding.png")
